8ac9e682bed0d763f51b3c9b697c51f164e81a93
David-Li0406 <2819238408@qq.com>
2021-09-10 14:28:55 +0800

    revise add_speicial_token.py

--- scripts/add_special_tokens.py
+++ scripts/add_special_tokens.py
@@ -1,10 +1,14 @@
 import json
 
-f = open('../models/gpt2-small/gpt2-vocab.json', 'r')
+f = open('../models/gpt2-small/vocab.json', 'r')
 vocab = json.load(f)
 f.close()
 vocab["<|bos|>"] = len(vocab)
+print(vocab["<|bos|>"])
 vocab["<|pad|>"] = len(vocab)
+print(vocab['<|pad|>'])
+vocab["<|sep|>"] = len(vocab)
+print(vocab["<|sep|>"])
 print(len(vocab))
 f = open('../models/gpt2-small/vocab.json', 'w')
 vocab = json.dump(vocab, f)
